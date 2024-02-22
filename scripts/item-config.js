c61c3f641b6f19912d15d9ffd3206c5221245c62
Twcash <triarch48@gmail.com>
2024-02-22 13:26:04 -0600

    Complete redo of everything

--- scripts/item-config.js
+++ scripts/item-config.js
@@ -2,18 +2,16 @@ Events.on(ClientLoadEvent, e => {
 const manganese = Vars.content.item("tantros-test-manganese");
 const godhelpme = Vars.content.item("tantros-test-bauxite");
 const duralumin = Vars.content.item("tantros-test-duralumin");
+const nitride = Vars.content.item("tantros-test-nitride");
+const gallium = Vars.content.item("tantros-test-gallium");
 const scrap = Items.scrap
 const sand = Items.sand
 const coal = Items.coal
 const oxide = Items.oxide
-const silicon = Items.silicon
 const lead = Items.lead
-const thorium = Items.thorium
 const sodium = Vars.content.item("tantros-test-sodium");
-const fissile = Vars.content.item("tantros-test-fissile");
-const plutonium = Vars.content.item("tantros-test-plutonium");
 const metaglass = Items.metaglass
-const tantrosOnlyItems = Seq.with(sand, manganese, godhelpme, oxide,  lead, sodium, coal, scrap, silicon, metaglass, duralumin, plutonium, fissile );
+const tantrosOnlyItems = Seq.with(sand, manganese, godhelpme, oxide,  lead, sodium, coal, scrap, metaglass, duralumin, nitride, gallium );
 
 Planets.tantros.hiddenItems.addAll(Vars.content.items()).removeAll(tantrosOnlyItems)
 })
