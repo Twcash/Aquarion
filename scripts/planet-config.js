413d0c284d1642dc376eb5fe56276f2d212f6cd0
Twcash <triarch48@gmail.com>
2024-03-15 20:25:45 -0500

    script merge

--- scripts/planet-config.js
+++
@@ -1,9 +0,0 @@
-Events.on(ClientLoadEvent, e => {
-Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
-Planets.tantros.allowLaunchToNumbered = false
-Planets.tantros.allowLaunchLoadout = false
-Planets.tantros.defaultCore = Vars.content.block("tantros-test-core-pike")
-Planets.tantros.generator.defaultLoadout = Schematics.read(Vars.tree.get("schematics/core-pike.msch"));
-});
-
-
