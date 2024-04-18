1d268271fb57138f365f0b6d4865a6a5cbee4e7a
Leo-MathGuy <leo.mathguy@gmail.com>
2024-04-18 22:52:18 +0500

    g

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,40 @@
-Log.info("Aquaria - Sending version check");
+Events.on(ClientLoadEvent, (event) => {
+    Log.info("Aquaria - Sending version check");
 
-var req = new Http.get(
-    "https://raw.githubusercontent.com/Twcash/Tantros-Test/main/mod.hjson",
-    (res) => {
-        var resp = res.getResultAsString();
-        Log.info("Aquaria - response: \n" + resp);
+    var req = new Http.get(
+        "https://raw.githubusercontent.com/Twcash/Tantros-Test/main/mod.hjson",
+        (res) => {
+            var resp = res.getResultAsString();
+            Log.info("Aquaria - response: \n" + resp);
 
-        var json = Jval.read(resp);
-        Log.info("Aquaria - remote ver: " + json.get("version"));
+            var json = Jval.read(resp);
+            Log.info("Aquaria - remote ver: " + json.get("version"));
 
-        var vers = Vars.mods.getMod("aquarion").meta.version;
-        Log.info("Aquaria - local version: " + vers);
+            var vers = Vars.mods.getMod("aquarion").meta.version;
+            Log.info("Aquaria - local version: " + vers);
 
-        if (!vers.equals(json.get("version"))) {
-            Log.warn("Aquaria - not up to date");
-            try {
-                Vars.ui.showOkText(
-                    "[#22CCFF]Aquarion[white]",
-                    "[green]Update available[white], please reinstall Aquaria for latest content!",
-                    () => {}
-                );
-            } catch (err) {
-                Log.info("Error: " + err.toString());
+            if (!vers.equals(json.get("version"))) {
+                Log.warn("Aquaria - not up to date");
+                try {
+                    Vars.ui.showOkText(
+                        "[#22CCFF]Aquarion[white]",
+                        "[green]Update available[white], please reinstall Aquaria for latest content!",
+                        () => {}
+                    );
+                } catch (err) {
+                    Log.info("Error: " + err.toString());
+                }
+            } else {
+                Log.info("Aquaria - up to date");
             }
-        } else {
-            Log.info("Aquaria - up to date");
+        },
+        (err) => {
+            Vars.ui.showOkText(
+                "[#22CCFF]Aquarion[white]",
+                "[red]ERROR:[white] Cannot check for updates!",
+                () => {}
+            );
+            Log.err("Aquaria - update check failed :(");
         }
-    },
-    (err) => {
-        Vars.ui.showOkText(
-            "[#22CCFF]Aquarion[white]",
-            "[red]ERROR:[white] Cannot check for updates!",
-            () => {}
-        );
-        Log.err("Aquaria - update check failed :(");
-    }
-);
+    );
+});
