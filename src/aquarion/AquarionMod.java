43b360b59083ebdef963023fcf2b21ea8f027d64
Norax <156533926+Noraxx1@users.noreply.github.com>
2024-07-04 04:34:45 +0200

    Update AquarionMod.java

--- src/aquarion/AquarionMod.java
+++ src/aquarion/AquarionMod.java
@@ -1,6 +1,16 @@
 package aquarion;
 
 import mindustry.mod.*;
+import aquarion.blocks.*;
 
 public class AquarionMod extends Mod {
+
+    @Override
+    public void loadContent() {
+        aquarionItems.loadContent();
+        aquarionLiquids.loadContent();
+        envBlocks.loadContent();
+        defenseBlocks.loadContent();
+        coreBlocks.loadContent();
+    }
 }
