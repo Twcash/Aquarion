db96083379e6107644ff4c54c326c648d4f461fc
Twcash <triarch48@gmail.com>
2024-07-08 18:48:21 -0500

    Sealed conveyors

--- src/aquarion/blocks/AquaDistribution.java
+++ src/aquarion/blocks/AquaDistribution.java
@@ -1,5 +1,6 @@
 package aquarion.blocks;
 
+import aquarion.world.blocks.distribution.SealedConveyor;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.*;
@@ -13,7 +14,7 @@ import static mindustry.type.ItemStack.with;
 
 public class AquaDistribution {
     // Armored and Sealed Conveyors
-    public static Block armoredSealedConveyor, sealedOverflow, sealedUnloader, sealedConveyor, sealedRouter, sealedSorter, sealedUnderflow;
+    public static Block armoredSealedConveyor, sealedOverflow, sealedUnloader, sealedConveyor, sealedRouter, sealedSorter, sealedUnderflow, sealedJunction;
 
     // Cargo
     public static Block cargoDepot, cargoDock, cargoTerminal;
@@ -28,30 +29,54 @@ public class AquaDistribution {
     public static Block manganeseBridge, manganeseConveyor;
 
     public static void loadContent() {
-        sealedConveyor = new Duct("sealedconveyor"){{
-            requirements(Category.distribution, with(lead, 3, sodium,2));
+        sealedConveyor = new SealedConveyor("sealed-conveyor"){{
+            requirements(Category.distribution, with(lead, 1, bauxite,1));
             envEnabled = 4;
             speed = 10F;
+            solid = false;
+            visualSpeed = 10;
+            underBullets = true;
+            stopSpeed = 70;
             buildCostMultiplier = 2f;
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
 
-        armoredSealedConveyor = new Duct("armored-sealed-conveyor"){{
-            requirements(Category.distribution, with(lead, 3, sodium,2));
+        armoredSealedConveyor = new SealedConveyor("armored-sealed-conveyor"){{
+            requirements(Category.distribution, with(lead, 2,bauxite, 1, sodium,1));
             speed = 10F;
+            health = 75;
             solid = true;
-            underBullets = false;
+            armored = true;
+            visualSpeed = 10;
+            underBullets = true;
+            stopSpeed = 70;
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
-        manganeseConveyor = new Conveyor("manganese-conveyor"){{
-            requirements(category.distribution, with(manganese, 1, gallium, 1));
-            displayedSpeed = 15.5f;
-            speed = 0.12f;
+
+        manganeseConveyor = new SealedConveyor("manganese-conveyor"){{
+            requirements(category.distribution, with(manganese, 1, gallium, 1, bauxite, 2));
+            speed = 4f;
+            stopSpeed = 45;
+            visualSpeed = 20;
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
+
+        sealedRouter = new Router("sealed-router"){{
+            requirements(category.distribution, with(lead, 10, bauxite, 5));
+            envEnabled |= Env.terrestrial | Env.underwater;
+            envDisabled |= Env.spores | Env.scorching;
+        }};
+        sealedJunction = new Junction("sealed-junction"){{
+            requirements(category.distribution, with(lead, 15, bauxite, 10, gallium, 5));
+            capacity = 8;
+            speed = 48;
+            envEnabled |= Env.terrestrial | Env.underwater;
+            envDisabled |= Env.spores | Env.scorching;
+        }};
+
         manganeseBridge = new BufferedItemBridge("manganese-bridge"){{
             requirements(category.distribution, with(gallium, 35, manganese, 15));
             consumePower(0.083f);
@@ -63,25 +88,25 @@ public class AquaDistribution {
             envDisabled |= Env.spores | Env.scorching;
         }};
 
-        sealedSorter = new Sorter("sealedsorter"){{
+        sealedSorter = new Sorter("sealed-sorter"){{
             requirements(category.distribution, with(lead, 5));
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
+
         sealedOverflow = new OverflowGate("sealed-overflow"){{
             requirements(category.distribution, with(lead, 5));
             invert = false;
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
+
         sealedUnderflow = new OverflowGate("sealed-underflow"){{
             requirements(category.distribution, with(lead, 5));
             invert = true;
             envEnabled |= Env.terrestrial | Env.underwater;
             envDisabled |= Env.spores | Env.scorching;
         }};
-
-
 
         // WARNING: this requires the unitType but theres none implemented yet...
         /* cargoDock = new UnitCargoLoader("cargo-dock"){{
