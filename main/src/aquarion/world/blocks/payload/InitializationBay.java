59aaccab1b11f28c73f35b46e89e7acd8c7545df
Twcash <131608563+Twcash@users.noreply.github.com>
2026-01-04 21:55:54 -0600

    rewrite of initializationBay

--- main/src/aquarion/world/blocks/payload/InitializationBay.java
+++ main/src/aquarion/world/blocks/payload/InitializationBay.java
@@ -2,13 +2,15 @@ package aquarion.world.blocks.payload;
 
 import aquarion.world.blocks.units.UnitBlock;
 import arc.Events;
+import arc.graphics.g2d.Draw;
+import arc.math.Mathf;
 import arc.math.geom.Vec2;
 import arc.util.Nullable;
 import mindustry.ai.UnitCommand;
 import mindustry.game.EventType;
-import mindustry.gen.Building;
-import mindustry.gen.PayloadUnit;
-import mindustry.gen.Unit;
+import mindustry.gen.*;
+import mindustry.graphics.Pal;
+import mindustry.ui.Bar;
 import mindustry.world.blocks.payloads.BuildPayload;
 import mindustry.world.blocks.payloads.Payload;
 import mindustry.world.blocks.payloads.PayloadBlock;
@@ -19,58 +21,87 @@ public class InitializationBay extends PayloadBlock {
     public InitializationBay(String name) {
         super(name);
         outputsPayload = acceptsPayload = true;
-
+        rotate = true;
     }
+
+    @Override
+    public void setBars(){
+        super.setBars();
+        addBar("progress", (initializeBuild e) -> new Bar("bar.progress", Pal.ammo, e::totProg));
+    }
+
     public class initializeBuild extends PayloadBlockBuild {
         public float progress = 0;
+        public float timeNeed = 1;
+
         public @Nullable Vec2 commandPos;
         public @Nullable UnitCommand command;
-
-
+        public float totProg(){
+            return progress/timeNeed;
+        }
         @Override
         public void updateTile() {
             super.updateTile();
-            if(payload == null) return;
-            moveInPayload();
-            if(payload instanceof BuildPayload pay){
-                if(pay.build instanceof UnitBlock.UnitBlockBuild unit){
-                    if(unit.block instanceof UnitBlock blok){
-                        float timeNeed = blok.time;
-                        progress += edelta()/(timeNeed/2f);
-                        if(progress >= blok.time){
-                            Unit unitb = blok.unit.create(this.team);
-                            if(unit.isCommandable()){
-                                if(commandPos != null){
-                                    unitb.command().commandPosition(commandPos);
-                                }
+            if (payload == null) return;
 
-                                unitb.command().command(command == null && unitb.type.defaultCommand != null ? unitb.type.defaultCommand : command);
+            if (payload instanceof BuildPayload) {
+                moveInPayload();
+            }
+            if (payload instanceof BuildPayload pay) {
+                if (pay.build instanceof UnitBlock.UnitBlockBuild unitBuild &&
+                        unitBuild.block instanceof UnitBlock blok) {
+                    timeNeed = blok.time;
+                    progress += edelta();
+                    if (progress >= blok.time) {
+                        Unit unit = blok.unit.create(team);
+
+                        if (unit.isCommandable()) {
+                            if (commandPos != null) {
+                                unit.command().commandPosition(commandPos);
                             }
 
-                            payload = new UnitPayload(unitb);
-
-                            progress = 0;
+                            unit.command().command(command != null ? command : unit.type.defaultCommand);
                         }
+                        payload = new UnitPayload(unit);
+                        progress = 0f;
                     }
                 } else {
-                    //KILL IT WITH FIRE
                     payload = null;
+                    progress = 0f;
                 }
-            } else if(payload instanceof UnitPayload t){
-                moveOutPayload();
-                Events.fire(new EventType.UnitCreateEvent(t.unit, this));
-                payVector.setZero();
-            }else {
-                //KILL IT WITH FIRE
+            } else if (payload instanceof UnitPayload unitPay) {
+                dumpPayload();
                 payload = null;
+                Sounds.blockExplodeElectric.at(unitPay.x(), unitPay.y(), Mathf.random(0.8f, 1f));
+                Events.fire(new EventType.UnitCreateEvent(unitPay.unit, this));
+                payVector.setZero();
             }
         }
+
         @Override
-        public boolean acceptPayload(Building source, Payload payload){
-            if(payload.size() <= this.block.size && this.payload == null && payload instanceof BuildPayload pay){
-                if(pay.build instanceof UnitBlock.UnitBlockBuild) return true;
-            };
-            return false;
+        public void draw(){
+            Draw.rect(region,x,y);
+            if(payload!= null) payload.draw();
+            Draw.rect(topRegion, x, y);
+            Draw.rect(outRegion,x,y, rotdeg());
+            for(int i = 0; i < 4; i++){
+                if(blends(i) && i != rotation){
+                    Draw.rect(inRegion, x, y, (i * 90f) - 180f);
+                }
+            }
         }
+//        @Override
+//        public boolean acceptPayload(Building source, Payload payload){
+//            if(this.payload != null) return false;
+//
+//            if(payload instanceof BuildPayload pay){
+//                if(pay.build instanceof UnitBlock.UnitBlockBuild unit){
+//                    return payload.size() <= block.size;
+//                }
+//            }
+//
+//            return false;
+//        }
+
     }
 }
