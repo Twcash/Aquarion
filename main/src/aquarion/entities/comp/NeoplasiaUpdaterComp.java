a3e5f2065ffdd4393cbd98f8fc79db8ef09d57ce
Twcash <131608563+Twcash@users.noreply.github.com>
2026-02-23 20:45:08 -0600

    stupider

--- main/src/aquarion/entities/comp/NeoplasiaUpdaterComp.java
+++ main/src/aquarion/entities/comp/NeoplasiaUpdaterComp.java
@@ -2,10 +2,11 @@ package aquarion.entities.comp;
 import aquarion.gen.NeoplasmManagerc;
 import aquarion.annotations.Annotations;
 import aquarion.world.entities.NeoplasmManager;
+import mindustry.gen.Entityc;
 
 @Annotations.EntityDef(value = NeoplasmManagerc.class, serialize = false, genio = false)
 @Annotations.EntityComponent
-public class NeoplasiaUpdaterComp {
+public class NeoplasiaUpdaterComp implements Entityc {
     public transient NeoplasmManager manager;
     @Override
     public void update(){
