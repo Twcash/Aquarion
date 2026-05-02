33a11590d25a69a3936b940986fc685295801298
Twcash <131608563+Twcash@users.noreply.github.com>
2026-05-02 09:50:05 -0500

    Update Pipe.java

--- main/src/aquarion/world/blocks/distribution/Pipe.java
+++ main/src/aquarion/world/blocks/distribution/Pipe.java
@@ -109,7 +109,7 @@ public class Pipe extends LiquidRouter implements Autotiler {
     public class PipeBuild extends LiquidRouterBuild {
         public int tiling = 0, blending;
         public int index1, index2, underBlending;
-        publiQueue<Building> queue = new LinkedList<>();
+        public Queue<Building> queue = new LinkedList<>();
         private final Set<Building> visited = new HashSet<>();
 
         public void equalizeLiquids() {
