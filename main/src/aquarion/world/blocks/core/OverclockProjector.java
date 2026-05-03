bb6e9823c0c9fd3dfdb478699ab25e8f62c5e7e8
Twcash <131608563+Twcash@users.noreply.github.com>
2026-05-02 19:24:33 -0500

    Update OverclockProjector.java

--- main/src/aquarion/world/blocks/core/OverclockProjector.java
+++ main/src/aquarion/world/blocks/core/OverclockProjector.java
@@ -18,7 +18,7 @@ import static mindustry.Vars.indexer;
 
 public class OverclockProjector extends OverdriveProjector{
 
-    public float damagePercent = 0.0025f;
+    public float damagePercent = 0.0015;
 
     public OverclockProjector(String name){
         super(name);
