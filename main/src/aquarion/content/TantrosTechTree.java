2a8f4a67c3b11d6e8fd4efb342c8e83ec123b3d2
VireVeonix <113903676+VireVeonix@users.noreply.github.com>
2026-05-10 22:10:49 +0200

    Update TantrosTechTree.java

--- main/src/aquarion/content/TantrosTechTree.java
+++ main/src/aquarion/content/TantrosTechTree.java
@@ -229,7 +229,7 @@ public class TantrosTechTree {
                                     new Objectives.Research(armoredSealedConveyor),
                                     new Objectives.Research(redact)
                             ), () -> {
-                                node(brineplateau, Seq.with(
+                                node(brinePlateau, Seq.with(
                                      new Objectives.SectorComplete(CrystalCaverns)
                                      ), ()->{});
                                 node(FeldsparRavine, Seq.with(
