20e2bcbce7014807fd0d3c6e9fb4381e5f604994
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-11 21:18:33 +0200

    Add SpaceReceiver block for item handling

---
+++ main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceReceiver.java
@@ -0,0 +1,39 @@
+package aquarion.world.blocks;
+
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.gen.Building;
+import mindustry.content.Fx;
+import mindustry.type.Item;
+import mindustry.world.Block;
+
+public class SpaceReceiver extends Block {
+
+    public SpaceReceiver(String name) {
+        super(name);
+        update = true;
+        solid = true;
+        hasItems = true;
+        itemCapacity = 500;
+    }
+
+    public class SpaceReceiverBuild extends Building {
+
+        public void handleIncomingItems(Item item, int amount) {
+            Fx.coreLand.at(x, y);
+            items.add(item, amount);
+        }
+
+        @Override
+        public void updateTile() {
+            if (items.total() > 0) {
+                dump(); 
+            }
+        }
+
+        @Override
+        public boolean acceptItem(Building source, Item item) {
+            return false; 
+        }
+    }
+}
