fa2dd3ecfb35cae395389b08b0ff826a9179627d
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-11 21:23:34 +0200

    Refactor launchResources method for clarity

--- main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceSender.java
+++ main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceSender.java
@@ -1,6 +1,5 @@
 package aquarion.world.blocks.distribution.spacetransfer;
 
-import arc.math.Mathf;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
 import mindustry.gen.Building;
@@ -8,6 +7,7 @@ import mindustry.gen.Groups;
 import mindustry.content.Fx;
 import mindustry.type.Item;
 import mindustry.world.Block;
+import aquarion.world.blocks.distribution.spacetransfer.*;
 
 public class SpaceSender extends Block {
     public int maxLaunchStorage = 100;
@@ -38,12 +38,14 @@ public class SpaceSender extends Block {
         public void launchResources() {
             Fx.launch.at(x, y);
 
-            // Безопасный поиск приёмника через группу всех построек на карте
             Groups.build.each(b -> {
-                if (b instanceof SpaceReceiver.SpaceReceiverBuild receiver && receiver.team == this.team) {
-                    items.each((item, amount) -> {
-                        receiver.handleIncomingItems(item, amount);
-                    });
+                if (b instanceof SpaceReceiver.SpaceReceiverBuild) {
+                    SpaceReceiver.SpaceReceiverBuild receiver = (SpaceReceiver.SpaceReceiverBuild) b;
+                    if (receiver.team == this.team) {
+                        items.each((item, amount) -> {
+                            receiver.handleIncomingItems(item, amount);
+                        });
+                    }
                 }
             });
             items.clear();
