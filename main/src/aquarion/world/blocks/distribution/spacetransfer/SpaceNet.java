ea58266735d75502c8a98d1a555ecfa76d8e62d7
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 16:17:37 +0200

    Add SpaceNet class for managing orbital cargo

---
+++ main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceNet.java
@@ -0,0 +1,28 @@
+package aquarion.world.blocks.distribution.spacetransfer;
+
+import arc.struct.IntMap;
+import mindustry.type.Item;
+import mindustry.world.modules.ItemModule;
+
+public class SpaceNet {
+    // Хранилище: ID сектора -> Предметы, которые сейчас летят к нему на орбиту
+    public static IntMap<ItemModule> orbitCargo = new IntMap<>();
+
+    public static void addCargo(int sectorId, Item item, int amount) {
+        if (!orbitCargo.containsKey(sectorId)) {
+            orbitCargo.put(sectorId, new ItemModule());
+        }
+        orbitCargo.get(sectorId).add(item, amount);
+    }
+
+    public static int getCargo(int sectorId, Item item) {
+        if (!orbitCargo.containsKey(sectorId)) return 0;
+        return orbitCargo.get(sectorId).get(item);
+    }
+
+    public static void removeCargo(int sectorId, Item item, int amount) {
+        if (orbitCargo.containsKey(sectorId)) {
+            orbitCargo.get(sectorId).remove(item, amount);
+        }
+    }
+}
