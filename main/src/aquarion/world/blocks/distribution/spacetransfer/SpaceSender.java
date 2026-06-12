2d112a5bab04971389593cdaffef0a9b4981de6b
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 09:44:21 +0200

    Refactor SpaceSender for resource management and drawing

--- main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceSender.java
+++ main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceSender.java
@@ -1,23 +1,35 @@
 package aquarion.world.blocks.distribution.spacetransfer;
 
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
+import mindustry.graphics.Pal;
 import mindustry.gen.Building;
 import mindustry.gen.Groups;
+import mindustry.content.Fx;
 import mindustry.type.Item;
+import mindustry.type.Liquid;
 import mindustry.world.Block;
+import mindustry.world.consumers.ConsumePower;
+import aquarion.content.AquaLiquids;
 import aquarion.world.blocks.distribution.spacetransfer.SpaceReceiver;
 
 public class SpaceSender extends Block {
-    public int maxLaunchStorage = 100;
     public float launchCooldown = 300f;
+    public float kerosenePerLaunch = 50f;
+    public float powerPerTick = 2f;
 
     public SpaceSender(String name) {
         super(name);
         update = true;
         solid = true;
         hasItems = true;
+        hasLiquids = true;
+        hasPower = true;
         configurable = true;
+        
+        consume(new ConsumePower(powerPerTick, 0.0f, false));
     }
 
     public class SpaceSenderBuild extends Building {
@@ -25,20 +37,43 @@ public class SpaceSender extends Block {
 
         @Override
         public void updateTile() {
-            if (items.total() >= maxLaunchStorage && progress >= launchCooldown) {
-                launchResources();
-                progress = 0;
-            }
-            if (progress < launchCooldown) {
-                progress += edelta();
+            boolean hasResources = items.total() >= itemCapacity && liquids.get(AquaLiquids.kerosene) >= kerosenePerLaunch;
+            boolean hasPowerSupply = power != null && power.status > 0;
+            boolean receiverReady = checkReceiverReady();
+
+            if (hasResources && hasPowerSupply && receiverReady) {
+                if (progress < launchCooldown) {
+                    progress += edelta();
+                } else {
+                    launchResources();
+                    progress = 0;
+                }
+            } else {
+                if (progress > 0) {
+                    progress -= edelta() * 0.5f;
+                }
             }
         }
 
-        public void launchResources() {
+        public boolean checkReceiverReady() {
+            final boolean[] ready = {false};
             Groups.build.each(b -> {
-                if (b instanceof SpaceReceiver.SpaceReceiverBuild) {
-                    SpaceReceiver.SpaceReceiverBuild receiver = (SpaceReceiver.SpaceReceiverBuild) b;
-                    if (receiver.team == this.team) {
+                if (b instanceof SpaceReceiver.SpaceReceiverBuild receiver) {
+                    if (receiver.team == this.team && receiver.canReceive()) {
+                        ready[0] = true;
+                    }
+                }
+            });
+            return ready[0];
+        }
+
+        public void launchResources() {
+            Fx.launch.at(x, y);
+            liquids.remove(AquaLiquids.kerosene, kerosenePerLaunch);
+
+            Groups.build.each(b -> {
+                if (b instanceof SpaceReceiver.SpaceReceiverBuild receiver) {
+                    if (receiver.team == this.team && receiver.canReceive()) {
                         items.each((item, amount) -> {
                             receiver.handleIncomingItems(item, amount);
                         });
@@ -49,8 +84,32 @@ public class SpaceSender extends Block {
         }
 
         @Override
+        public void drawSelect() {
+            super.drawSelect();
+            float barWidth = size * 8f;
+            float barHeight = 4f;
+            float bx = x - barWidth / 2f;
+            float by = y + (size * 8f) / 2f + 4f;
+
+            Draw.color(Pal.darkerGray);
+            Fill.rect(x, by + barHeight / 2f, barWidth, barHeight);
+            
+            if (progress > 0) {
+                Draw.color(Pal.accent);
+                float currentWidth = (barWidth - 2f) * (progress / launchCooldown);
+                Fill.rect(bx + 1f + currentWidth / 2f, by + barHeight / 2f, currentWidth, barHeight - 2f);
+            }
+            Draw.reset();
+        }
+
+        @Override
         public boolean acceptItem(Building source, Item item) {
-            return items.total() < maxLaunchStorage && items.get(item) < getMaximumAccepted(item);
+            return items.total() < itemCapacity;
+        }
+
+        @Override
+        public boolean acceptLiquid(Building source, Liquid liquid) {
+            return liquid == AquaLiquids.kerosene && liquids.get(liquid) < liquidCapacity;
         }
 
         @Override
