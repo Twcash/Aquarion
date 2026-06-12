c2b0331fbb197d07dc74c3c80795a18e90dbd25d
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 08:38:59 +0200

    Refactor reflection usage in AquaCategories

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -1,15 +1,13 @@
 package aquarion.content;
 
 import mindustry.type.Category;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 
 public class AquaCategories {
     public static Category aquaCategory;
 
     public static void init() {
         try {
-            Constructor<Category> constructor = Category.class.getDeclaredConstructor(String.class, int.class);
+            java.lang.reflect.Constructor<Category> constructor = Category.class.getDeclaredConstructor(String.class, int.class);
             constructor.setAccessible(true);
             
             int newId = Category.all.length;
@@ -19,12 +17,11 @@ public class AquaCategories {
             System.arraycopy(Category.all, 0, newCategories, 0, newId);
             newCategories[newId] = aquaCategory;
 
-            Field allField = Category.class.getDeclaredField("all");
+            java.lang.reflect.Field allField = Category.class.getDeclaredField("all");
             allField.setAccessible(true);
             allField.set(null, newCategories);
-
         } catch (Exception e) {
-            System.err.println("Failed to inject custom category via Reflection: " + e.getMessage());
+            System.err.println("Failed to inject custom category: " + e.getMessage());
             aquaCategory = Category.distribution;
         }
     }
