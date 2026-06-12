d1d7690fdefe80eaa32fad21bc467b02c4888f1c
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 08:21:30 +0200

    Refactor AquaCategories to use reflection for category

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -1,14 +1,26 @@
 package aquarion.content;
 
-import arc.util.EnumHelper;
 import mindustry.type.Category;
+import java.lang.reflect.Constructor;
 
 public class AquaCategories {
     public static Category aquaCategory;
 
     public static void init() {
         try {
-            aquaCategory = EnumHelper.add(Category.class, "aquaCategory");
+            Constructor<Category> constructor = Category.class.getDeclaredConstructor(String.class, int.class);
+            constructor.setAccessible(true);
+            
+            int id = Category.all.length;
+            aquaCategory = constructor.newInstance("aquaCategory", id);
+
+            Category[] newCategories = new Category[id + 1];
+            System.arraycopy(Category.all, 0, newCategories, 0, id);
+            newCategories[id] = aquaCategory;
+
+            java.lang.reflect.Field allField = Category.class.getDeclaredField("all");
+            allField.setAccessible(true);
+            allField.set(null, newCategories);
         } catch (Exception e) {
             throw new RuntimeException("Custom Category failed to initialize in v158.1: " + e.getMessage(), e);
         }
