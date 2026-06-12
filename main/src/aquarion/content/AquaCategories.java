3ab0fa2fbdb21c1d8601edc34f3c94534a1407de
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 08:26:48 +0200

    Refactor AquaCategories to use new ID handling

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -2,6 +2,7 @@ package aquarion.content;
 
 import mindustry.type.Category;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
 public class AquaCategories {
     public static Category aquaCategory;
@@ -11,18 +12,20 @@ public class AquaCategories {
             Constructor<Category> constructor = Category.class.getDeclaredConstructor(String.class, int.class);
             constructor.setAccessible(true);
             
-            int id = Category.all.length;
-            aquaCategory = constructor.newInstance("aquaCategory", id);
+            int newId = Category.all.length;
+            aquaCategory = constructor.newInstance("aquaCategory", newId);
 
-            Category[] newCategories = new Category[id + 1];
-            System.arraycopy(Category.all, 0, newCategories, 0, id);
-            newCategories[id] = aquaCategory;
+            Category[] newCategories = new Category[newId + 1];
+            System.arraycopy(Category.all, 0, newCategories, 0, newId);
+            newCategories[newId] = aquaCategory;
 
-            java.lang.reflect.Field allField = Category.class.getDeclaredField("all");
+            Field allField = Category.class.getDeclaredField("all");
             allField.setAccessible(true);
             allField.set(null, newCategories);
+
         } catch (Exception e) {
-            throw new RuntimeException("Custom Category failed to initialize in v158.1: " + e.getMessage(), e);
+            System.err.println("Failed to inject custom category via Reflection: " + e.getMessage());
+            aquaCategory = Category.distribution;
         }
     }
 }
