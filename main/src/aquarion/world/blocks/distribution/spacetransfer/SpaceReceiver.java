ab4ceffd81cdef27923d1bf5b8a531b2117eca37
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-12 09:44:50 +0200

    Enhance SpaceReceiver to handle liquids and items

--- main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceReceiver.java
+++ main/src/aquarion/world/blocks/distribution/spacetransfer/SpaceReceiver.java
@@ -3,22 +3,32 @@ package aquarion.world.blocks.distribution.spacetransfer;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
 import mindustry.gen.Building;
+import mindustry.content.Fx;
+import mindustry.content.Liquids;
 import mindustry.type.Item;
+import mindustry.type.Liquid;
 import mindustry.world.Block;
 
 public class SpaceReceiver extends Block {
+    public float waterPerReceive = 40f;
 
     public SpaceReceiver(String name) {
         super(name);
         update = true;
         solid = true;
         hasItems = true;
-        itemCapacity = 500;
+        hasLiquids = true;
     }
 
     public class SpaceReceiverBuild extends Building {
 
+        public boolean canReceive() {
+            return liquids.get(Liquids.water) >= waterPerReceive && items.total() < itemCapacity;
+        }
+
         public void handleIncomingItems(Item item, int amount) {
+            Fx.spawn.at(x, y); 
+            liquids.remove(Liquids.water, waterPerReceive);
             items.add(item, amount);
         }
 
@@ -32,6 +42,11 @@ public class SpaceReceiver extends Block {
         @Override
         public boolean acceptItem(Building source, Item item) {
             return false; 
+        }
+
+        @Override
+        public boolean acceptLiquid(Building source, Liquid liquid) {
+            return liquid == Liquids.water && liquids.get(liquid) < liquidCapacity;
         }
     }
 }
