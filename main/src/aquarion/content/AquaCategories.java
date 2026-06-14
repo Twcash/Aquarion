403ea4d867f18ededeedcc7338433e8691cabdb6
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-14 09:20:12 +0200

    Refactor AquaCategories to use reflection for initialization

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -3,6 +3,9 @@ package aquarion.content;
 import arc.util.Reflect;
 import mindustry.type.Category;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
 public class AquaCategories {
 
     public static Category refinery;
@@ -15,17 +18,25 @@ public class AquaCategories {
         initialized = true;
 
         try {
-            refinery = new Category("refinery");
-            heat = new Category("heat");
-
             Category[] original = Category.all;
+            int nextOrdinal = original.length;
+
+            refinery = Reflect.constructor(Category.class, String.class, int.class).newInstance("refinery", nextOrdinal);
+            heat = Reflect.constructor(Category.class, String.class, int.class).newInstance("heat", nextOrdinal + 1);
+
             Category[] extended = new Category[original.length + 2];
             System.arraycopy(original, 0, extended, 0, original.length);
-            
             extended[original.length] = refinery;
             extended[original.length + 1] = heat;
 
             Reflect.set(Category.class, "all", extended);
+
+            for (Field f : Category.class.getDeclaredFields()) {
+                if (f.getType() == Category[].class && !f.getName().equals("all")) {
+                    Reflect.set(Category.class, f.getName(), extended);
+                    break;
+                }
+            }
 
         } catch (Exception e) {
             throw new RuntimeException("Custom Category failed to initialize", e);
