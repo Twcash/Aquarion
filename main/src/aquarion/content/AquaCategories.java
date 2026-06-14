39bc83ef56fb184232b475d14d3d445817abd011
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-14 09:29:06 +0200

    Refactor AquaCategories to extend Category functionality

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -1,8 +1,8 @@
 package aquarion.content;
 
-import arc.util.Reflect;
 import mindustry.type.Category;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -18,43 +18,73 @@ public class AquaCategories {
         initialized = true;
 
         try {
-            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
-            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
-            unsafeField.setAccessible(true);
-            Object unsafe = unsafeField.get(null);
+            Category[] original = Category.all;
+            int nextOrdinal = original.length;
+            Category[] extended = new Category[original.length + 2];
+            System.arraycopy(original, 0, extended, 0, original.length);
 
-            Method allocateInstance = unsafeClass.getMethod("allocateInstance", Class.class);
-            Method objectFieldOffset = unsafeClass.getMethod("objectFieldOffset", Field.class);
-            Method putObject = unsafeClass.getMethod("putObject", Object.class, long.class, Object.class);
-            Method putInt = unsafeClass.getMethod("putInt", Object.class, long.class, int.class);
+            try {
+                Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+                Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+                unsafeField.setAccessible(true);
+                Object unsafe = unsafeField.get(null);
 
-            refinery = (Category) allocateInstance.invoke(unsafe, Category.class);
-            heat = (Category) allocateInstance.invoke(unsafe, Category.class);
+                Method allocateInstance = unsafeClass.getMethod("allocateInstance", Class.class);
+                Method objectFieldOffset = unsafeClass.getMethod("objectFieldOffset", Field.class);
+                Method putObject = unsafeClass.getMethod("putObject", Object.class, long.class, Object.class);
+                Method putInt = unsafeClass.getMethod("putInt", Object.class, long.class, int.class);
+                Method staticFieldOffset = unsafeClass.getMethod("staticFieldOffset", Field.class);
+                Method staticFieldBase = unsafeClass.getMethod("staticFieldBase", Field.class);
 
-            Field nameField = Enum.class.getDeclaredField("name");
-            long nameOffset = (long) objectFieldOffset.invoke(unsafe, nameField);
-            Field ordinalField = Enum.class.getDeclaredField("ordinal");
-            long ordinalOffset = (long) objectFieldOffset.invoke(unsafe, ordinalField);
+                refinery = (Category) allocateInstance.invoke(unsafe, Category.class);
+                heat = (Category) allocateInstance.invoke(unsafe, Category.class);
 
-            Category[] oldValues = Category.all;
-            int nextOrdinal = oldValues.length;
+                Field nameField = Enum.class.getDeclaredField("name");
+                long nameOffset = (long) objectFieldOffset.invoke(unsafe, nameField);
+                Field ordinalField = Enum.class.getDeclaredField("ordinal");
+                long ordinalOffset = (long) objectFieldOffset.invoke(unsafe, ordinalField);
 
-            putObject.invoke(unsafe, refinery, nameOffset, "refinery");
-            putInt.invoke(unsafe, refinery, ordinalOffset, nextOrdinal);
-            putObject.invoke(unsafe, heat, nameOffset, "heat");
-            putInt.invoke(unsafe, heat, ordinalOffset, nextOrdinal + 1);
+                putObject.invoke(unsafe, refinery, nameOffset, "refinery");
+                putInt.invoke(unsafe, refinery, ordinalOffset, nextOrdinal);
+                putObject.invoke(unsafe, heat, nameOffset, "heat");
+                putInt.invoke(unsafe, heat, ordinalOffset, nextOrdinal + 1);
 
-            Category[] newValues = new Category[oldValues.length + 2];
-            System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
-            newValues[oldValues.length] = refinery;
-            newValues[oldValues.length + 1] = heat;
+                extended[original.length] = refinery;
+                extended[original.length + 1] = heat;
 
-            Reflect.set(Category.class, "all", newValues);
+                Field allField = Category.class.getDeclaredField("all");
+                long allOffset = (long) staticFieldOffset.invoke(unsafe, allField);
+                Object allBase = staticFieldBase.invoke(unsafe, allField);
+                putObject.invoke(unsafe, allBase, allOffset, extended);
 
-            for (Field f : Category.class.getDeclaredFields()) {
-                if (f.getType() == Category[].class && !f.getName().equals("all")) {
-                    Reflect.set(Category.class, f.getName(), newValues);
-                    break;
+                for (Field f : Category.class.getDeclaredFields()) {
+                    if (f.getType() == Category[].class && !f.getName().equals("all")) {
+                        long fOffset = (long) staticFieldOffset.invoke(unsafe, f);
+                        Object fBase = staticFieldBase.invoke(unsafe, f);
+                        putObject.invoke(unsafe, fBase, fOffset, extended);
+                        break;
+                    }
+                }
+            } catch (Throwable e) {
+                Constructor<Category> constructor = Category.class.getDeclaredConstructor(String.class, int.class);
+                constructor.setAccessible(true);
+
+                refinery = constructor.newInstance("refinery", nextOrdinal);
+                heat = constructor.newInstance("heat", nextOrdinal + 1);
+
+                extended[original.length] = refinery;
+                extended[original.length + 1] = heat;
+
+                Field allField = Category.class.getDeclaredField("all");
+                allField.setAccessible(true);
+                allField.set(null, extended);
+
+                for (Field f : Category.class.getDeclaredFields()) {
+                    if (f.getType() == Category[].class && !f.getName().equals("all")) {
+                        f.setAccessible(true);
+                        f.set(null, extended);
+                        break;
+                    }
                 }
             }
 
