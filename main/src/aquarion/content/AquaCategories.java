75fa4957d00a9d1feebdee92698cf46bc2bf80ce
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-06-14 09:25:53 +0200

    Refactor AquaCategories to use Unsafe for category creation

--- main/src/aquarion/content/AquaCategories.java
+++ main/src/aquarion/content/AquaCategories.java
@@ -3,8 +3,8 @@ package aquarion.content;
 import arc.util.Reflect;
 import mindustry.type.Category;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class AquaCategories {
 
@@ -18,24 +18,51 @@ public class AquaCategories {
         initialized = true;
 
         try {
-            Category[] original = Category.all;
-            int nextOrdinal = original.length;
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            Object unsafe = unsafeField.get(null);
 
-            refinery = Reflect.makeConstructor(Category.class, String.class, int.class).newInstance("refinery", nextOrdinal);
-            heat = Reflect.makeConstructor(Category.class, String.class, int.class).newInstance("heat", nextOrdinal + 1);
+            Method allocateInstance = unsafeClass.getMethod("allocateInstance", Class.class);
+            Method objectFieldOffset = unsafeClass.getMethod("objectFieldOffset", Field.class);
+            Method putObject = unsafeClass.getMethod("putObject", Object.class, long.class, Object.class);
+            Method putInt = unsafeClass.getMethod("putInt", Object.class, long.class, int.class);
 
-            Category[] extended = new Category[original.length + 2];
-            System.arraycopy(original, 0, extended, 0, original.length);
-            extended[original.length] = refinery;
-            extended[original.length + 1] = heat;
+            refinery = (Category) allocateInstance.invoke(unsafe, Category.class);
+            heat = (Category) allocateInstance.invoke(unsafe, Category.class);
 
-            Reflect.set(Category.class, "all", extended);
+            Field nameField = Enum.class.getDeclaredField("name");
+            long nameOffset = (long) objectFieldOffset.invoke(unsafe, nameField);
+            Field ordinalField = Enum.class.getDeclaredField("ordinal");
+            long ordinalOffset = (long) objectFieldOffset.invoke(unsafe, ordinalField);
+
+            Category[] oldValues = Category.all;
+            int nextOrdinal = oldValues.length;
+
+            putObject.invoke(unsafe, refinery, nameOffset, "refinery");
+            putInt.invoke(unsafe, refinery, ordinalOffset, nextOrdinal);
+            putObject.invoke(unsafe, heat, nameOffset, "heat");
+            putInt.invoke(unsafe, heat, ordinalOffset, nextOrdinal + 1);
+
+            Category[] newValues = new Category[oldValues.length + 2];
+            System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
+            newValues[oldValues.length] = refinery;
+            newValues[oldValues.length + 1] = heat;
+
+            Reflect.set(Category.class, "all", newValues);
 
             for (Field f : Category.class.getDeclaredFields()) {
                 if (f.getType() == Category[].class && !f.getName().equals("all")) {
-                    Reflect.set(Category.class, f.getName(), extended);
+                    Reflect.set(Category.class, f.getName(), newValues);
                     break;
                 }
+            }
+
+            try {
+                Field constantDir = Class.class.getDeclaredField("enumConstantDirectory");
+                constantDir.setAccessible(true);
+                constantDir.set(Category.class, null);
+            } catch (Exception ignored) {
             }
 
         } catch (Exception e) {
