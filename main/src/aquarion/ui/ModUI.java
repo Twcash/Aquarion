3447f059ee550b3341c3cb356d3052f48206e49b
NikolayKot02 <svgbv1212@gmail.com>
2026-06-25 15:30:47 +0200

    add image to music

--- main/src/aquarion/ui/ModUI.java
+++ main/src/aquarion/ui/ModUI.java
@@ -4,17 +4,29 @@ import arc.Core;
 import arc.math.Interp;
 import arc.scene.actions.Actions;
 import arc.scene.event.Touchable;
+import arc.scene.ui.Image;
 import arc.scene.ui.layout.Table;
 import arc.util.Align;
 import mindustry.ui.Styles;
 
 public class ModUI {
     public static void showBottomToast(String text) {
+        showBottomToast(text, null);
+    }
+
+    public static void showBottomToast(String text, String iconName) {
         float dur = 1f;
 
         Table t = new Table(Styles.black3);
         t.touchable = Touchable.disabled;
-        t.margin(10).add(text).labelAlign(Align.center).style(Styles.outlineLabel);
+        t.margin(10);
+
+        if (iconName != null && !iconName.isEmpty() && Core.atlas.has(iconName)) {
+            t.add(new Image(Core.atlas.find(iconName))).size(32f).padRight(8f).left();
+        }
+
+        t.add(text).labelAlign(Align.center).style(Styles.outlineLabel).left();
+
         t.actions(
                 Actions.moveToAligned(Core.graphics.getWidth()/2f, -40, Align.center),
                 Actions.delay(0.5f),
