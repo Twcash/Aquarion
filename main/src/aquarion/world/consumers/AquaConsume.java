48d4070efd8e7601f80aac24d27691984b84c219
Twcash <131608563+Twcash@users.noreply.github.com>
2026-07-25 15:21:39 -0700

    Update AquaConsume.java

--- main/src/aquarion/world/consumers/AquaConsume.java
+++ main/src/aquarion/world/consumers/AquaConsume.java
@@ -136,10 +136,10 @@ public class AquaConsume extends Consume {
             } else if(e.consumer instanceof ConsumeItemFilter CIF){
                 Boolf<Item> filter = CIF.filter;
                 stats.add(Stat.booster, StatValues.items(stats.timePeriod, filter));
-            } else if(e.consumer instanceof ConsumeItemEfficiency CIE){
-                @Nullable ObjectFloatMap<Item> itemDurationMultipliers = CIE.itemDurationMultipliers;
-                stats.add(Stat.booster, StatValues.itemEffMultiplier(this::itemEfficiencyMultiplier, stats.timePeriod, filter, itemDurationMultipliers));
-            }
+            } //else if(e.consumer instanceof ConsumeItemEfficiency CIE){
+                //@Nullable ObjectFloatMap<Item> itemDurationMultipliers = CIE.itemDurationMultipliers;
+                //stats.add(Stat.booster, StatValues.itemEffMultiplier(this::itemEfficiencyMultiplier, stats.timePeriod, filter, itemDurationMultipliers));
+            //}
         }
     }
 
