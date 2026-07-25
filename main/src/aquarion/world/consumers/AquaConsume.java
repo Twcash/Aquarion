2fe4e387571444189ea080f2ea42e868856e2064
Twcash <131608563+Twcash@users.noreply.github.com>
2026-07-25 14:34:15 -0700

    It’s prolly gonna yell at me for this

--- main/src/aquarion/world/consumers/AquaConsume.java
+++ main/src/aquarion/world/consumers/AquaConsume.java
@@ -133,6 +133,9 @@ public class AquaConsume extends Consume {
                 for(ItemStack is : ci.items){
                     stats.add(stat, entryTable(is.item, is.amount, e.multiplier, timePeriod, !e.required, false));
                 }
+            } else if(e.consumer instanceof ConsumeItemFilter CIF){
+                Boolf<Item> filter = CIF.filter;
+                stats.add(Stat.input, StatValues.items(stats.timePeriod, filter));
             }
         }
     }
