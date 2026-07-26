c7972ae404975c293cadb9b8e87e87dbde566989
VireVeonix <113903676+VireVeonix@users.noreply.github.com>
2026-07-27 00:20:04 +0200

    Update AquaConsume.java

--- main/src/aquarion/world/consumers/AquaConsume.java
+++ main/src/aquarion/world/consumers/AquaConsume.java
@@ -160,7 +160,7 @@ public class AquaConsume extends Consume {
                 b.defaults().pad(5).left();
 
                 iconObjs.forEach( img ->{
-                    b.add(displayItem((mindustry.type.Item)img, baseAmount, timePeriod, true)).pad(2f).left().setWrap(true);
+                    b.add(displayItem((mindustry.type.Item)img, baseAmount, timePeriod, true)).pad(2f).left().Wrap();
                 });
                 b.add(booster ? "[accent]Booster" : "[gray]Required").pad(10f).padRight(4f).right();
             }).growX().pad(3).row();
