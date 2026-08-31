a0690d1f561060822dd45a9e31a610d0864f61be
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-08-31 07:58:08 +0200

    Adjust download button width in AquaMenuDialog
    Increased the width of the download button from 140f to 150f.

--- main/src/aquarion/ui/AquaMenuDialog.java
+++ main/src/aquarion/ui/AquaMenuDialog.java
@@ -291,7 +291,7 @@ public class AquaMenuDialog extends BaseDialog {
                     // Кнопка Скачать
                     actions.button(Core.bundle.get("aquarion.menu.download_release", "Download"), Icon.download, () -> {
                         Core.app.openURI(finalDownloadUrl);
-                    }).width(140f).height(actionBtnHeight).padRight(8f);
+                    }).width(150f).height(actionBtnHeight).padRight(8f);
 
                     // Кнопка GitHub
                     actions.button(Core.bundle.get("aquarion.menu.open_release_tag", "View on GitHub"), Icon.export, () -> {
